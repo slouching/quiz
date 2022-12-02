c3d69deedb577728f6ccda3f60254fa4678adb65
Slouching <dulnik@yandex.ru>
2022-12-02 11:12:37 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      Init
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const sequelize = require('./db')
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
